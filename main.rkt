45180dedae8c2854e255c7e5bdc298b01089214a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-11 09:12:15 +0800

    cleanup main.rkt

--- main.rkt
+++ main.rkt
@@ -3,22 +3,7 @@
 (module+ main
   (require racket/cmdline)
 
-  (define who (make-parameter "world"))
   (command-line
     #:program "no"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (who name)]
-    #:args ()
-    (printf "hello ~a~n" (who))))
-
-(module+ test
-  (require rackunit)
-
-  (define expected 1)
-  (define actual 1)
-
-  (test-case
-    "Example Test"
-    (check-equal? actual expected))
-
-  (test-equal? "Shortcut Equal Test" actual expected))
+    #:args (file)
+    (void)))
