a8c1038b689cb515800df913c2e30ab8a95af303
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-11 09:11:07 +0800

    lexer parser from still-compiling

---
+++ lexer.rkt
@@ -0,0 +1,227 @@
+#lang racket
+
+(provide lex lexer-tokens
+         (struct-out token)
+         (struct-out pos))
+
+; export
+(define (lex name input-port)
+  (define l (lexer name
+                   input-port
+                   ; state
+                   lex-white-space
+                   ; line column
+                   1 0
+                   ; start offset
+                   0 0
+                   (make-channel)))
+  (thread (λ () (run l)))
+  l)
+
+(define keyword*
+  '("true" "false" "and" "or"))
+
+; state functions
+(define (lex-white-space l)
+  (let loop ([c (peek l)])
+    (when (and (not (eof-object? c))
+               (or (char-whitespace? c)
+                   (end-of-line? c)))
+      (next l)
+      (loop (peek l))))
+  (ignore l)
+
+  (match (peek l)
+    [(? eof-object?) (emit l 'EOF)
+                     #f]
+    [#\+ (next l)
+         (emit l 'add)
+         lex-white-space]
+    [#\- (next l)
+         (emit l 'sub)
+         lex-white-space]
+    [#\* (next l)
+         (emit l 'mul)
+         lex-white-space]
+    [#\/ (next l)
+         (emit l 'div)
+         lex-white-space]
+    [#\^ (next l)
+         (emit l '^)
+         lex-white-space]
+    [#\= (next l)
+         (emit l 'eq)
+         lex-white-space]
+    [(? char-numeric?) lex-number]
+    [(? alpha-numeric?) lex-identifier]
+    [c (error 'unknown "don't know what to do with: `~a`" c)]))
+
+(define (lex-identifier l)
+  (let loop ([c (peek l)])
+    (when (alpha-numeric? c)
+      (next l)
+      (loop (peek l))))
+
+  (emit l 'identifier)
+  lex-white-space)
+
+(define (lex-number l)
+  (when (not (scan-number? l))
+    (error 'bad-number-syntax "bad number syntax: `~a`"
+           (peek-string (- (lexer-offset l) (lexer-start l))
+                        0 (lexer-input l))))
+  (emit l 'number)
+  lex-white-space)
+
+; helpers
+(struct pos (filename line column) #:transparent)
+
+(struct lexer
+  (name
+   input ; list char
+   state
+   ; for report position as format: (line, column)
+   line column
+   ; lexing helpers
+   offset start
+   tokens)
+  #:mutable
+  #:transparent)
+
+(struct token (typ val start-pos end-pos) #:transparent)
+
+(define (run lexer)
+  (when (lexer-state lexer)
+    (set-lexer-state! lexer
+                      ((lexer-state lexer) lexer))
+    (run lexer)))
+
+(define (next l)
+  (set-lexer-offset! l (add1 (lexer-offset l)))
+  (set-lexer-column! l (add1 (lexer-column l)))
+  (define c (peek-char (lexer-input l)
+                       (- (lexer-offset l) (lexer-start l))))
+  (if (eof-object? c)
+      c
+      (when (end-of-line? c)
+        (set-lexer-line! l (add1 (lexer-line l)))
+        (set-lexer-column! l 0))))
+
+(define (peek l) (peek-char (lexer-input l)
+                            (- (lexer-offset l) (lexer-start l))))
+
+(define (new-item l ty value)
+  (channel-put
+   (lexer-tokens l)
+   (token ty value
+          ; since token must on the same line, we can rely on length of value
+          (pos (lexer-name l) (lexer-line l) (- (lexer-column l) (string-length value)))
+          (pos (lexer-name l) (lexer-line l) (lexer-column l)))))
+
+(define keyword=>keyword-type
+  (make-hash
+   (map (λ (word)
+          (cons word (string->symbol word)))
+        keyword*)))
+(define (emit l ty)
+  (define value
+    (read-string (- (lexer-offset l) (lexer-start l))
+                 (lexer-input l)))
+  (match value
+    [(? eof-object?) (new-item l 'EOF value)]
+    [else (if (hash-ref keyword=>keyword-type value #f)
+              (new-item l (hash-ref keyword=>keyword-type value) value)
+              (new-item l ty value))])
+  (set-lexer-start! l (lexer-offset l)))
+
+(define (accept? l valid)
+  (cond
+    [(and (char? (peek l))
+          (string-contains? valid (string (peek l))))
+     (next l)
+     #t]
+    [else #f]))
+
+(define (accept-run l valid)
+  (let loop ([c (peek l)])
+    (when (and (char? c) (string-contains? valid (string c)))
+      (next l)
+      (loop (peek l)))))
+
+(define (ignore l)
+  (read-string (- (lexer-offset l) (lexer-start l))
+               (lexer-input l))
+  (set-lexer-start! l (lexer-offset l)))
+
+(define (scan-number? l)
+  (define digits "0123456789")
+  (when (and (accept? l "0") (accept? l "xX"))
+    (set! digits "0123456789abcdefABCDEF"))
+  (accept-run l digits)
+  (when (accept? l ".")
+    (accept-run l digits))
+  (when (accept? l "eE")
+    (accept? l "+-")
+    (accept-run l "0123456789"))
+  ; Next thing mustn't be alphanumeric.
+  (cond
+    [(alpha-numeric? (peek l))
+     (next l)
+     #f]
+    [else #t]))
+
+(define (alpha-numeric? c)
+  (and (char? c)
+       (or (char-ci=? c #\_)
+           (char-alphabetic? c)
+           (char-numeric? c))))
+
+(define (end-of-line? c)
+  (match c
+    [#\newline #t]
+    [else #f]))
+
+; Test
+(module+ test
+  (require rackunit)
+
+  (define (test-pos l c)
+    (pos "test" l c))
+
+  (test-case "lexing"
+             (define l (lex "test" (open-input-string "31+12 abc")))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'number "31" (test-pos 1 0) (test-pos 1 2)))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'add "+" (test-pos 1 2) (test-pos 1 3)))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'number "12" (test-pos 1 3) (test-pos 1 5)))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'identifier "abc" (test-pos 1 6) (test-pos 1 9))))
+
+  (test-case "spacing"
+             (define l (lex "test" (open-input-string "  abc")))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'identifier "abc" (test-pos 1 2) (test-pos 1 5))))
+
+  (test-case "keywords"
+             (define l (lex "test" (open-input-string "true false")))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'true "true" (test-pos 1 0) (test-pos 1 4)))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'false "false" (test-pos 1 5) (test-pos 1 10))))
+
+  (test-case "longer expression"
+             (define l (lex "test" (open-input-string "1 + 2 * 3")))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'number "1" (test-pos 1 0) (test-pos 1 1)))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'add "+" (test-pos 1 2) (test-pos 1 3)))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'number "2" (test-pos 1 4) (test-pos 1 5)))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'mul "*" (test-pos 1 6) (test-pos 1 7)))
+             (check-equal? (channel-get (lexer-tokens l))
+                           (token 'number "3" (test-pos 1 8) (test-pos 1 9))))
+
+  )
---
+++ parser.rkt
@@ -0,0 +1,126 @@
+#lang racket
+
+(require "lexer.rkt")
+
+; ast
+(struct expr () #:transparent)
+(struct binary expr (op left right) #:transparent)
+
+; parse
+(define (parse-expr p left-hand-side previous-primary)
+  (define lhs (if left-hand-side
+                  left-hand-side
+                  (parse-unary p)))
+
+  (let loop ([lookahead (peek p)])
+    (when (>= (precedence lookahead) previous-primary)
+      (define operator lookahead)
+      (take p)
+      (define rhs (parse-unary p))
+      (set! lookahead (peek p))
+      (let loop ()
+        (when (or (> (precedence lookahead) (precedence operator))
+                  (and (right-assoc? lookahead)
+                       (= (precedence lookahead) (precedence operator))))
+          (set! rhs (parse-expr p rhs (precedence lookahead)))
+          (set! lookahead (peek p))
+          (loop)))
+      (set! lhs (binary (token-typ operator)
+                        lhs rhs))
+      (loop lookahead)))
+
+  lhs)
+
+(define (parse-unary p)
+  (define tok (peek p))
+  (case (token-typ tok)
+    [(number) (take p)
+              (string->number (token-val tok))]
+    [(true) (take p)
+            'true]
+    [(false) (take p)
+             'false]
+    [(identifier) (take p)
+                  (token-val tok)]
+    [else (error 'unknown "~a" tok)]))
+
+; helper
+(struct parser (name lexer tokens offset)
+  #:mutable
+  #:transparent)
+
+(define (parse name input)
+  (define lexer (lex name input))
+  (define p (parser name lexer (stream) 0))
+  (parse-expr p #f 1))
+
+(define (peek p [n 0])
+  (get-token p (+ (parser-offset p) n)))
+(define (take p)
+  (define origin (parser-offset p))
+  (set-parser-offset! p (add1 origin))
+  (get-token p origin))
+(define (consume p . wants)
+  (predict p wants))
+(define (predict p . wants)
+  (for ([i (length wants)]
+        [want wants])
+    (define tok (peek p i))
+    (unless (eq? (token-typ tok) want)
+      (error 'unexpected-token "want ~a, got ~a" want (token-typ tok)))))
+
+(define (get-token p fixed-offset)
+  (when (stream-empty? (parser-tokens p))
+    (increase-token-stream p))
+  (define tokens (parser-tokens p))
+  (if (>= fixed-offset (stream-length tokens))
+      (let ([last-token (stream-ref tokens (sub1 (stream-length tokens)))])
+        (case (token-typ last-token)
+          [(EOF) last-token]
+          [else (increase-token-stream p)
+                (get-token p fixed-offset)]))
+      (stream-ref tokens fixed-offset)))
+(define (increase-token-stream p)
+  (define l (parser-lexer p))
+  (define new-last-token (channel-get (lexer-tokens l)))
+  (set-parser-tokens! p
+                      (stream-append (parser-tokens p) (stream new-last-token))))
+
+(define (right-assoc? token)
+  (case (token-typ token)
+    [(^) #t]
+    [else #f]))
+(define (precedence token)
+  (define op** '((eq)
+                 (and or)
+                 (add sub)
+                 (mul div ^)))
+  (define m (make-hash))
+  (for ([i (length op**)]
+        [op* op**])
+    (for ([op op*])
+      (hash-set! m op (+ 2 i))))
+  (hash-ref m (token-typ token) 0))
+
+(module+ test
+  (require rackunit)
+
+  (check-equal? (parse "parsing" (open-input-string "12 + 23 * 34"))
+                (binary 'add 12 (binary 'mul 23 34)))
+
+  (test-case "increase token stream automatically"
+             (define (test-pos l c)
+               (pos "" l c))
+             (define lexer (lex "" (open-input-string "12 + 23 * 34")))
+             (define p (parser "" lexer (stream) 0))
+             (check-equal? (get-token p 4)
+                           (token 'number "34" (test-pos 1 10) (test-pos 1 12))))
+
+  (test-case "right assoc"
+             (check-equal? (parse "parsing" (open-input-string "12 ^ 23 ^ 34"))
+                           (binary '^ 12 (binary '^ 23 34))))
+
+  (check-equal? (parse "parsing" (open-input-string "true and true = true or false"))
+                (binary 'eq
+                        (binary 'and 'true 'true)
+                        (binary 'or 'true 'false))))
