22c5d5f25754a623c13c2fc4e49dd1b0d5807efe
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-17 19:56:46 +0800

    cmd(cut out debug mode which print all statements)

--- main.rkt
+++ main.rkt
@@ -1,12 +1,21 @@
 #lang racket
 
+(require "parser.rkt"
+         "eval.rkt")
+
+(define/match (handle args)
+  [((list "run" file))
+   (define p (make-parser file (open-input-file file)))
+   (eval-module (parse-module p))]
+  [((list "debug" file))
+   (define p (make-parser file (open-input-file file)))
+   (for ([s (parse-module p)])
+     (printf "statement: ~a\n" s))])
+
 (module+ main
-  (require racket/cmdline
-           "parser.rkt"
-           "eval.rkt")
+  (require racket/cmdline)
 
   (command-line
     #:program "no"
-    #:args (file)
-    (define p (make-parser file (open-input-file file)))
-    (eval-module (parse-module p))))
+    #:args args
+    (handle args)))
