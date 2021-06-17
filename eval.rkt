79d93f95f9bf73283c312737b939d16b5a9cdb1d
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-17 19:57:14 +0800

    eval(function call)

--- eval.rkt
+++ eval.rkt
@@ -3,16 +3,35 @@
 (provide eval-module)
 
 (require "ast.rkt"
+         "lexer.rkt"
          "env.rkt")
 
 (define (eval-module ss)
   (for ([s ss])
-    (match s
-      [(vardef name expr)
-       (env/bind name expr)]
-      [(fndef name params body)
-       (env/bind name body)]
-      [expr (displayln (eval-expr expr))])))
+    (cond
+      [(statement? s)
+       (eval-stmt s)]
+      [(expr? s) (displayln (eval-expr s))])))
+
+(define (eval-stmt s [return #f])
+  (match s
+    [(vardef name expr)
+     (env/bind (token-val name) expr)]
+    [(fndef name params body)
+     (env/bind (token-val name)
+         (lambda (args)
+           (let/cc return
+             (parameterize ([cur-env (make-env)])
+               (for ([p params]
+                     [a args])
+                 (env/bind p a))
+                 (for ([s body])
+               (eval-stmt s return))))))]
+    [(ret e)
+     (unless return
+       (error 'cannot-return "You're not in a context can return, e.g. function"))
+     (return (eval-expr e))]
+    [(? expr?) (eval-expr s)]))
 
 (define (eval-expr e)
   (match e
@@ -22,4 +41,5 @@
        [(-) (- (eval-expr l) (eval-expr r))]
        [(*) (* (eval-expr l) (eval-expr r))]
        [(/) (/ (eval-expr l) (eval-expr r))])]
+    [(func-call fn args) ((env/lookup fn) args)]
     [else e]))
