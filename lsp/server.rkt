9974d4a229a795c4436e86f4b8bfb9c9d7ebe6f3
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-19 00:43:40 +0800

    lsp(implement all by self)

--- lsp/server.rkt
+++ lsp/server.rkt
@@ -2,9 +2,11 @@
 
 (provide run-server)
 
-(require jsonrpc
-         lsp
+(require lsp
          web-server/servlet-env
+         web-server/http/request-structs
+         web-server/http/json
+         json
          "../lexer.rkt"
          "../parser.rkt"
          "../ast.rkt")
@@ -16,14 +18,14 @@
   (Range #:start (convert-pos start)
          #:end (convert-pos end)))
 
-(define/match (user-handler method params)
-  [("initialize" params)
+(define/match (user-handler method params succ err)
+  [("initialize" params s e)
    (initialize params)]
-  [("textDocument/documentHighlight" params)
+  [("textDocument/documentHighlight" params s e)
    (define path (uri->path (hash-ref (hash-ref params 'textDocument) 'uri)))
    (define l (lex path (open-input-file path)))
    (define highlights '())
-   (let loop ([tok (channel-get (lexer-tokens l))])
+   (succ (let loop ([tok (channel-get (lexer-tokens l))])
      (match (token-typ tok)
        ['EOF highlights]
        ['identifier
@@ -43,10 +45,9 @@
                       (range (token-start-pos tok)
                              (token-end-pos tok))
                       #f))))
-        (loop (channel-get (lexer-tokens l)))]))]
-  [(method params)
-   (printf "not implement ~a" method)
-   params])
+        (loop (channel-get (lexer-tokens l)))])))]
+  [(method params s e)
+   (error 'fail "not implement ~a" method)])
 
 (define (run-server)
   (serve/servlet
@@ -54,3 +55,30 @@
    #:port 4389
    #:launch-browser? #f
    #:servlet-path "/jsonrpc"))
+
+(define (jsonrpc-start jsonrpc-handler)
+  (λ (request)
+    (define json-str (request-post-data/raw request))
+    (match json-str
+      [#f
+       (response/jsexpr
+        (hasheq 'jsonrpc "2.0"
+                'id "0"
+                'result '()))]
+      [json-str
+       (define json (bytes->jsexpr json-str))
+       (response/jsexpr
+         (jsonrpc-handler (hash-ref json 'method)
+                          (hash-ref json 'params)
+                          (succ json) (err json)))])))
+
+(define (succ json)
+  (lambda (result)
+    (hasheq 'jsonrpc "2.0"
+            'id (hash-ref json 'id)
+            'result result)))
+(define (err json)
+  (lambda (e)
+    (hasheq 'jsonrpc "2.0"
+            'id (hash-ref json 'id)
+            'error e)))
