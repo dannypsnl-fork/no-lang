1bbecbcc00a4339d025b819819213a579e35b84a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-19 13:04:10 +0800

    lsp(stdio)

--- lsp/server.rkt
+++ lsp/server.rkt
@@ -7,6 +7,8 @@
          web-server/http/request-structs
          web-server/http/json
          json
+         mzlib/cml
+         racket/exn
          "../lexer.rkt"
          "../parser.rkt"
          "../ast.rkt")
@@ -18,67 +20,135 @@
   (Range #:start (convert-pos start)
          #:end (convert-pos end)))
 
-(define/match (user-handler method params succ err)
-  [("initialize" params s e)
-   (initialize params)]
-  [("textDocument/documentHighlight" params s e)
-   (define path (uri->path (hash-ref (hash-ref params 'textDocument) 'uri)))
-   (define l (lex path (open-input-file path)))
-   (define highlights '())
-   (succ (let loop ([tok (channel-get (lexer-tokens l))])
-     (match (token-typ tok)
-       ['EOF highlights]
-       ['identifier
-        (set! highlights
-              (append
-               highlights
-               (list (DocumentHighlight
-                      (range (token-start-pos tok)
-                             (token-end-pos tok))
-                      DocumentHighlightKind.Read))))
-        (loop (channel-get (lexer-tokens l)))]
-       [typ
-        (set! highlights
-              (append
-               highlights
-               (list (DocumentHighlight
-                      (range (token-start-pos tok)
-                             (token-end-pos tok))
-                      #f))))
-        (loop (channel-get (lexer-tokens l)))])))]
-  [(method params s e)
-   (error 'fail "not implement ~a" method)])
+;; https://www.cs.utah.edu/plt/publications/pldi04-ff.pdf
+
+(struct Q (in-ch out-ch mgr-t))
+
+(define (queue)
+  (define in-ch (channel))
+  (define out-ch (channel))
+  (define (serve msgs)
+    (cond [(empty? msgs)
+           (serve (list (sync (channel-recv-evt in-ch))))]
+          [else
+           (sync (choice-evt
+                  (wrap-evt
+                   (channel-recv-evt in-ch)
+                   (λ (m)
+                     (serve (append msgs (list m)))))
+                  (wrap-evt
+                   (channel-send-evt out-ch (first msgs))
+                   (thunk*
+                    (serve (rest msgs))))))]))
+  (define mgr-t (spawn (λ () (serve empty))))
+  (Q in-ch out-ch mgr-t))
+
+(define (queue-send-evt q v)
+  (guard-evt
+   (λ ()
+     (thread-resume (Q-mgr-t q) (current-thread))
+     (channel-send-evt (Q-in-ch q) v))))
+
+(define (queue-recv-evt q)
+  (guard-evt
+   (λ ()
+     (thread-resume (Q-mgr-t q) (current-thread))
+     (channel-recv-evt (Q-out-ch q)))))
+
+(define (report-error exn)
+  (eprintf "\nCaught exn:\n~a\n" (exn->string exn)))
+
+(define already-initialized? #f)
+(define already-shutdown? #f)
+(define (shutdown id)
+  (set! already-shutdown? #t)
+  (success-response id (json-null)))
+(define ((report-request-error id method) exn)
+  (eprintf "Caught exn in request ~v\n~a\n" method (exn->string exn))
+  (define err (format "internal error in method ~v" method))
+  (error-response id INTERNAL-ERROR err))
+
+(define (process-notification method params)
+  (match method
+    ["exit"
+     (exit (if already-shutdown? 0 1))]
+    ["textDocument/didOpen"
+     (error 'fixme)]
+    ["textDocument/didClose"
+     (error 'fixme)]
+    ["textDocument/didChange"
+     (error 'fixme)]
+    [_ (void)]))
+
+(define (process-message msg)
+  (match msg
+    ;; Request
+    [(hash-table ['id (? (or/c number? string?) id)]
+                 ['method (? string? method)])
+     (define params (hash-ref msg 'params hasheq))
+     (define response (process-request id method params))
+     (display-message/flush response)]
+    ;; Notification
+    [(hash-table ['method (? string? method)])
+     (define params (hash-ref msg 'params hasheq))
+     (process-notification method params)]
+    ;; Batch Request
+    [(? (non-empty-listof (and/c hash? jsexpr?)))
+     (for-each process-message msg)]
+    ;; Invalid Message
+    [_
+     (define id-ref (hash-ref msg 'id void))
+     (define id (if ((or/c number? string?) id-ref) id-ref (json-null)))
+     (define err "The JSON sent is not a valid request object.")
+     (display-message/flush (error-response id INVALID-REQUEST err))]))
+;; Processes a request. This procedure should always return a jsexpr
+;; which is a suitable response object.
+(define (process-request id method params)
+  (with-handlers ([exn:fail? (report-request-error id method)])
+    (match method
+      ["initialize"
+       (initialize id params)]
+      ["shutdown"
+       (shutdown id)]
+      ["textDocument/hover"
+       (error 'unimplement)]
+      ["textDocument/completion"
+       (error 'unimplement)]
+      ["textDocument/signatureHelp"
+       (error 'unimplement)]
+      ["textDocument/definition"
+       (error 'unimplement)]
+      ["textDocument/documentHighlight"
+       (error 'unimplement)]
+      ["textDocument/references"
+       (error 'unimplement)]
+      ["textDocument/documentSymbol"
+       (error 'unimplement)]
+      ["textDocument/rename"
+       (error 'unimplement)]
+      ["textDocument/prepareRename"
+       (error 'unimplement)]
+      ["textDocument/formatting"
+       (error 'unimplement) ]
+      [_
+       (eprintf "invalid request for method ~v\n" method)
+       (define err (format "The method ~v was not found" method))
+       (error-response id METHOD-NOT-FOUND err)])))
 
 (define (run-server)
-  (serve/servlet
-   (jsonrpc-start user-handler)
-   #:port 4389
-   #:launch-browser? #f
-   #:servlet-path "/jsonrpc"))
-
-(define (jsonrpc-start jsonrpc-handler)
-  (λ (request)
-    (define json-str (request-post-data/raw request))
-    (match json-str
-      [#f
-       (response/jsexpr
-        (hasheq 'jsonrpc "2.0"
-                'id "0"
-                'result '()))]
-      [json-str
-       (define json (bytes->jsexpr json-str))
-       (response/jsexpr
-         (jsonrpc-handler (hash-ref json 'method)
-                          (hash-ref json 'params)
-                          (succ json) (err json)))])))
-
-(define (succ json)
-  (lambda (result)
-    (hasheq 'jsonrpc "2.0"
-            'id (hash-ref json 'id)
-            'result result)))
-(define (err json)
-  (lambda (e)
-    (hasheq 'jsonrpc "2.0"
-            'id (hash-ref json 'id)
-            'error e)))
+  (define q (queue))
+  (define (consume)
+    (define msg (sync (queue-recv-evt q)))
+    (match msg
+      ['parse-json-error
+       (define err "Invalid JSON was received by the server.")
+       (display-message/flush (error-response (json-null) PARSE-ERROR err))]
+      [_
+       (with-handlers ([exn:fail? report-error])
+         (process-message msg))])
+    (consume))
+  (spawn consume)
+  (for ([msg (in-port read-message)])
+    (sync (queue-send-evt q msg)))
+  (eprintf "Unexpected EOF\n")
+  (exit 1))
