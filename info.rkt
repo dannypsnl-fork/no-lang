8f510ed755d4389b5646568fc265901fe732f1a9
Lîm Tsú-thuàn≡悃悃 <dannypsnl@gmail.com>
2021-06-20 16:22:51 +0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "no")
-(define deps '("base" "jsonrpc" "lsp"))
+(define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/no.scrbl" ())))
 (define pkg-desc "Description Here")
